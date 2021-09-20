a736f7718cb4f7a28e19f9ab7c31e90fc3921796
AnimeSam <sphung@uncc.edu>
2021-09-19 20:04:50 -0400

    Set downloaded to true if live_download ran successfully

--- index.py
+++ index.py
@@ -7,8 +7,22 @@ import live_download
 
 FETCHED_JSON = "fetched.json"
 fetched = {}
-
-
+"""
+fetched = {
+    "https://youtube.com/watch?v=aXdH11XtikI": {
+        "downloaded": "false",
+        "timestamp": 1631826398.5934074
+    },
+    "https://youtube.com/watch?v=wri4zMtI5sc": {
+        "downloaded": "false",
+        "timestamp": 1631826398.5934074
+    },
+    "https://youtube.com/watch?v=ebI3UIEJ3YM": {
+        "downloaded": "false",
+        "timestamp": 1631829482.364617
+    }
+}
+"""
 def save():
     with open(FETCHED_JSON, "w", encoding="utf8") as f:
         print(fetched)
@@ -41,7 +55,7 @@ if os.path.isfile(FETCHED_JSON):
             fetched = json.load(f)
         except json.JSONDecodeError as j:
             print(j)
-            fetched = {member_links()}
+            fetched = {}
             save()
 else:
     fetched = {}
@@ -56,12 +70,21 @@ def main():
                             'timestamp': time.time()}
     save()
 
-    private_download.download(fetched.keys())
-
+    download_result = live_download.download(fetched.keys())
+    for download in download_result:
+        if fetched[download]:
+            # set fetched video's downloaded key to download's key
+            fetched[download]['downloaded'] = download_result[download]
+    save()
 
 
 if __name__ == '__main__':
-    main()
     clear_link()
-    time.sleep(10)
-    print(time.time())
+    while True:
+        start_time = time.time()
+        if time.time() - start_time> 43200:
+            clear_link()
+            start_time = time.time()
+        main()
+        # change sleep time to 1 min maybe
+        time.sleep(30)
---
+++ live_download.py
@@ -0,0 +1,27 @@
+import subprocess
+
+setDownloaded = {}
+def download(video_id):
+    for video in video_id:
+        command_list = ['start', 'cmd', '/k']
+        command_list += ['ytarchive.py', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt', '-o',
+                         "NoArchive\%(channel)s\%(upload_date)s - %(title)s\%(upload_date)s - %(title)s (%(id)s)",
+                         '--add-metadata', '-t', '--vp9', '--write-description', '--write-thumbnail', '--threads', '2',
+                         '-w']
+        command_list += ['yt-dlp', video]
+        # remove later test line vvvvv
+        # command_list += ['yt-dlp', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt', video]
+        try:
+            print("[INFO] Downloading Live Stream")
+            output = subprocess.run(command_list, shell=True)
+            # If theres an error then this ensures a redownload, but only works if the program crashes by itself immediately
+            # print("[Debug] Output: ", output)
+            print("[Debug] Immediate Return Code:", output.returncode)
+            if output.returncode != 0:
+                setDownloaded[video] = "false"
+            setDownloaded[video] = "true"
+        except Exception as e:
+            print(e)
+            setDownloaded[video] = "false"
+    return setDownloaded
+
