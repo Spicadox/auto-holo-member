3e46a1bfc350594d8dbaca1806196837baf325aa
AnimeSam <sphung@uncc.edu>
2021-10-04 20:52:24 -0400

    Use yt-dlp to write-info-json

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 import json
 import os
@@ -9,14 +10,17 @@ fetched = {}
 """
 fetched = {
     "https://youtube.com/watch?v=aXdH11XtikI": {
+        "id": "aXdH11XtikI",
         "downloaded": "false",
         "timestamp": 1631826398.5934074
     },
     "https://youtube.com/watch?v=wri4zMtI5sc": {
+        "id": "wri4zMtI5sc",
         "downloaded": "false",
         "timestamp": 1631826398.5934074
     },
     "https://youtube.com/watch?v=ebI3UIEJ3YM": {
+        "id": "ebI3UIEJ3YM",
         "downloaded": "false",
         "timestamp": 1631829482.364617
     }
@@ -28,6 +32,7 @@ def save():
         json.dump(fetched, f, indent=4, ensure_ascii=False)
         print("Saving json")
 
+
 def clear_link():
     with open(FETCHED_JSON, encoding="utf8") as f:
         fetched = json.load(f)
@@ -38,6 +43,8 @@ def clear_link():
                 removal.append(link)
         for link in removal:
             print("Expired, Removing " + link)
+            if os.path.isfile(f"{os.getcwd()}\jsons\{id}.json"):
+                os.remove(f"{os.getcwd()}\jsons\{id}.info.json")
             del fetched[link]
             print(fetched)
     # Saving json if links can be removed
@@ -60,17 +67,29 @@ else:
     fetched = {}
     save()
 
+def create_json(id):
+    command_args = ['yt-dlp', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt']
+    command_args += ['--skip-download', '--write-info-json', '-o', f'{os.getcwd()}\\jsons\\{id}', id]
+    return subprocess.run(command_args, shell=True)
 
 def main():
     links = member_links()
     for link in links:
         if link not in fetched.keys():
-            fetched[link] = {'downloaded': 'false',
+            fetched[link] = {'id': link[-11:],
+                            'downloaded': 'false',
                             'timestamp': time.time()}
     save()
     # Get video_id's that have not been downloaded
     download_id = []
     for link in fetched:
+        try:
+            print("Downloading json...")
+            process = create_json(fetched[link]['id'])
+            print("Return Code: ", process.returncode)
+        except Exception as e:
+            print(e, "\nError creating Json")
+
         if fetched[link]['downloaded'] == 'false':
             download_id.append(link)
     # Download the undownloaded the member video if there are videos to download
