18c7473d17bd39f046eb4a97c57eba81d1808995
Sam Phung <sphung@uncc.edu>
2022-01-09 18:31:33 -0500

    Return an array of tuples and use const variables

--- live_download.py
+++ live_download.py
@@ -1,27 +1,31 @@
 import subprocess
+import const
 
-setDownloaded = {}
-def download(video_id):
-    for video in video_id:
+setDownloaded = []
+
+
+def download(videos):
+    for video in videos:
+        channel_name = video[0]
+        video_id = video[1]
         # /c - close console after execution
         command_list = ['start', 'cmd', '/c']
-        command_list += ['ytarchive.exe', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt', '-o',
-                         "NoArchive\%(channel)s\%(upload_date)s - %(title)s\%(upload_date)s - %(title)s (%(id)s)",
+        command_list += ['ytarchive.exe', '--cookies', const.COOKIE, '-o',
+                         const.DOWNLOAD,
                          '--add-metadata', '-t', '--vp9', '--write-description', '--write-thumbnail', '--threads', '2',
                          '-w']
-        command_list += [f'https://www.youtube.com/watch?v={video}', 'best']
+        command_list += [f'https://www.youtube.com/watch?v={video_id}', 'best']
         # remove later test line
         # command_list += ['yt-dlp', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt', video]
         try:
-            print(f"[INFO] Downloading Live Stream {video}")
             output = subprocess.run(command_list, shell=True)
             # If theres an error then this ensures a redownload, but only works if the program crashes by itself immediately
             # print("[Debug] Output: ", output)
             if output.returncode != 0:
-                setDownloaded[video] = "false"
-            setDownloaded[video] = "true"
+                setDownloaded.append((channel_name, "false"))
+            setDownloaded.append((channel_name, "true"))
         except Exception as e:
             print(e)
-            setDownloaded[video] = "false"
+            setDownloaded.append((channel_name, "false"))
     return setDownloaded
 
