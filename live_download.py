63f35b99f0a6a6ca42f810a3537cbd38035bdf1b
Sam Phung <sphung@uncc.edu>
2022-01-23 01:03:51 -0500

    Allow downloading for linux system by checking whether the OS is Windows or Linux. This is intended to satisfy changes from the pi branch

--- live_download.py
+++ live_download.py
@@ -1,5 +1,6 @@
 import subprocess
 import const
+import platform
 
 setDownloaded = []
 
@@ -8,15 +9,23 @@ def download(videos):
     for video in videos:
         channel_name = video[0]
         video_id = video[1]
-        # /c - close console after execution
-        command_list = ['start', 'cmd', '/c']
-        command_list += ['ytarchive.exe', '--cookies', const.COOKIE, '-o',
-                         const.DOWNLOAD,
-                         '--add-metadata', '-t', '--vp9', '--write-description', '--write-thumbnail', '--threads', '2',
-                         '-w']
-        command_list += [f'https://www.youtube.com/watch?v={video_id}', 'best']
-        # remove later test line
-        # command_list += ['yt-dlp', '--cookies', 'I:\\archive scripts\\batch scripts\\member_script\\newcookiefile_2.txt', video]
+
+        if platform.system() == "Window":
+            # /c - close console after execution
+            command_list = ['start', 'cmd', '/c']
+            command_list += ['ytarchive.exe', '--cookies', const.COOKIE, '-o',
+                             const.DOWNLOAD,
+                             '--add-metadata', '-t', '--vp9', '--write-description', '--write-thumbnail', '--threads', '2',
+                             '-w']
+            command_list += [f'https://www.youtube.com/watch?v={video_id}', 'best']
+        else:
+            # else if it's a Linux system
+            command_list = ['lxterminal', '-e', 'python3']
+            command_list += ['ytarchive.exe', '-o', const.DOWNLOAD,
+                             '--add-metadata', '-t', '--ipv6', '--vp9', '--write-description', '--write-thumbnail',
+                             '--threads', '1',
+                             '-w']
+            command_list += [f'https://www.youtube.com/watch?v={video_id}', 'best']
         try:
             output = subprocess.run(command_list, shell=True)
             # If theres an error then this ensures a redownload, but only works if the program crashes by itself immediately
