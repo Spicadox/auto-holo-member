c470038fb734755a3ac8aa328b3634f0800dbb99
Sam Phung <sphung@uncc.edu>
2022-02-02 00:58:07 -0500

    Added option to use youtube-dl by default to fetch member's only streams

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import time
 import json
 import os
 import urllib
+import youtube_dl
 from member_link import member_links
 import log
 import live_download
@@ -12,6 +13,15 @@ import const
 WEBHOOK_URL = const.WEBHOOK_URL
 if const.LOGGING:
     logger = log.create_logger("logfile.log")
+try:
+    with open('channels.json', 'r', encoding="utf8") as f:
+        channels = json.load(f)
+except json.JSONDecodeError as jsonError:
+    logger.error(jsonError)
+    channels = {}
+except ValueError as valuerror:
+    logger.error(valuerror)
+    channels = {}
 FETCHED_JSON = "fetched.json"
 fetched = {}
 """
@@ -70,6 +80,44 @@ else:
     save()
 
 
+def get_latest_member_streams():
+    logger.info("Getting member streams using yt_dl...")
+    dict_list = []
+    ytdl_format_options = {
+        "playlistend": 1,
+        "ignoreerrors": True,
+        "logger": logger,
+        "quiet": True,
+        "no_warnings": True,
+        "cookiefile": const.COOKIE
+    }
+    with youtube_dl.YoutubeDL(ytdl_format_options) as ytdl:
+        for channel in channels.values():
+            try:
+                member_url = f"https://www.youtube.com/channel/{channel}/membership"
+                logger.debug(member_url)
+                info_dict = ytdl.extract_info(member_url, download=False)
+                #logger.debug(info_dict)
+
+                if info_dict is None:
+                    continue
+
+                try:
+                    if info_dict['entries'][0]['is_live']:
+                        dict_list.append({"id": info_dict['entries'][0]['id'], "title": info_dict['entries'][0]['title'],
+                                          "channel": {"name": info_dict['entries'][0]['uploader']}})
+                    else:
+                        continue
+                except Exception as e:
+                    logger.debug(e)
+                    continue
+                time.sleep(4)
+            except Exception as e:
+                logger.error(e)
+                continue
+    return dict_list
+
+
 def get_links():
     other_channel_ids = const.other_channel_ids
     api_key = const.API_KEY
@@ -82,30 +130,34 @@ def get_links():
             logger.debug(email_links)
     except Exception as e:
         logger.error(e)
+
+    if const.FETCH_FROM_YTDL:
+        member_page_links = get_latest_member_streams()
+
     # Use holodex api to grab live streams
-    try:
-        req = requests.get(url="https://holodex.net/api/v2/live?org=Hololive&status=live").json()
-    except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
-        logger.error(rerror)
-        logger.error("Something went wrong sending the request")
-        req = []
-        pass
-    try:
-        if other_channel_ids is not None or len(other_channel_ids) > 0:
-            if api_key is not None:
-                req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
-            else:
-                req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
-    except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
-        logger.error(rerror)
-        logger.error("Something went wrong sending the request")
-        req2 = []
-        pass
+    req = []
+    req2 = []
+    if const.FETCH_FROM_HOLODEX:
+        try:
+            req = requests.get(url="https://holodex.net/api/v2/live?org=Hololive&status=live").json()
+        except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
+            logger.error(rerror)
+            logger.error("Something went wrong sending the request")
+        try:
+            if other_channel_ids is not None or len(other_channel_ids) > 0:
+                if api_key is not None:
+                    req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
+                else:
+                    req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
+        except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
+            logger.error(rerror)
+            logger.error("Something went wrong sending the request")
 
     combined_data = req + req2 + email_links
     streams = []
     if len(combined_data) != 0:
         # TODO: download info-json using yt-dlp or save json like auto-ytarchive
+        logger.debug(combined_data)
         for stream in combined_data:
             logger.debug(stream)
             # Check the html page to see if it's a member stream
@@ -113,13 +165,12 @@ def get_links():
             #     steams.append(stream)
             # Return name of the stream in lowercase
             stream_name = stream['title'].lower()
-            logger.debug(stream_name)
-            logger.debug(re.search('(member|members)', stream_name))
-            logger.debug(re.search('(メンバ|メン限)', stream_name))
+            logger.debug(f"Stream name: {stream_name}")
             member_only = get_is_member_stream(stream['id'])
             logger.debug(member_only)
             if re.search('(member|members)', stream_name) or re.search('(メンバ|メン限)', stream_name) or member_only:
                 streams.append(stream)
+    streams += member_page_links
     return streams
 
 
@@ -155,10 +206,12 @@ def download():
         if stream["channel"]["name"] not in fetched.keys():
             fetched[stream["channel"]["name"]] = {'id': stream["id"],
                                                   'downloaded': 'false',
+                                                  'notified': 'false',
                                                   'timestamp': time.time()}
-
-            if WEBHOOK_URL is not None:
+            print(fetched)
+            if WEBHOOK_URL is not None and fetched[stream["channel"]["name"]]["notified"] != 'true':
                 notify(streamer_name, stream_id)
+                fetched[stream["channel"]["name"]]['notified'] = 'true'
     save()
     # Get video_id's that have not been downloaded
     download_id = []
@@ -188,6 +241,7 @@ def download():
 
 
 if __name__ == '__main__':
+    logger.info("Starting Program")
     clear_link()
     expire_time = const.EXPIRE_TIME
     sleep_time = const.SLEEP_TIME
