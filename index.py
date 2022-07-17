04c038ab37d9772b2e233da3e9356db5528c8dca
Sam Phung <sphung@uncc.edu>
2022-07-17 19:40:22 -0400

    Removed unused methods and switch to only obtain member's streams through embedded yt-dlp. Added a means to check the status of VOD after it went live and send status to the dead member webhook. Added means to get json file and chat given the private util.py file is found. Fixed lives so that it is properly removed after it has ended. Added loading text.

--- index.py
+++ index.py
@@ -1,21 +1,22 @@
+import threading
 import time
 import json
 import os
-from member_link import member_links
+from requests.adapters import HTTPAdapter
 import log
 import live_download
 import requests
 import re
-import subprocess
 import const
-# try:
-#     import getjson
-# except ImportError or ModuleNotFoundError:
-#     pass
+import yt_dlp
+
 
 WEBHOOK_URL = const.WEBHOOK_URL
+DEAD_MEMBER_WEBHOOK_URL = const.DEAD_MEMBER_WEBHOOK_URL
+
 if const.LOGGING:
     logger = log.create_logger("logfile.log")
+    ytdlp_logger = log.YTLogger
 try:
     with open('channels.json', 'r', encoding="utf8") as f:
         channels = json.load(f)
@@ -33,6 +34,7 @@ fetched = {
             "dDT1_BkJdKQ": {
                 "downloaded": "true",
                 "notified": "true",
+                "status": "OK",
                 "timestamp": 1652023702.6788592
             }
     }
@@ -45,14 +47,14 @@ def save():
         json.dump(fetched, writeFile, indent=4, ensure_ascii=False)
 
 
-def clear_link():
+def clear_link(expire_time):
     with open(FETCHED_JSON, encoding="utf8") as readFile:
         fetched_json = json.load(readFile)
 
         removal = []
         for link in fetched_json.keys():
             for id in fetched_json[link].keys():
-                if time.time() - fetched_json[link][id]['timestamp'] > 28800:
+                if time.time() - fetched_json[link][id]['timestamp'] > expire_time:
                     removal.append((link, id))
         for link in removal:
             # Remove link in global fetched and local fetched_json
@@ -70,8 +72,7 @@ def clear_link():
     if len(removal) > 0:
         with open(FETCHED_JSON, "w", encoding="utf8") as writeFile:
             json.dump(fetched_json, writeFile, indent=4, ensure_ascii=False)
-            logger.info("Saving json")
-            print(" " * 40, end="\n")
+            # logger.info("Saving json")
 
 
 if os.path.isfile(FETCHED_JSON):
@@ -91,125 +92,67 @@ def get_latest_member_streams():
     dict_list = []
     counter = 1
     dict_length = len(channels)
-    for channel in channels.values():
-        try:
-            print(f"[INFO] Looking through member's pages {counter}/{dict_length}...", end="\r")
-            command_list = ["yt-dlp", "--cookies-from-browser", "chrome", "--playlist-end", "1", "-j",
-                            f"https://www.youtube.com/channel/{channel}/membership"]
-            process = subprocess.run(command_list, capture_output=True, text=True)
-            # logger.debug(process)
+    ydl_opts = {"quiet": True, "no_warnings": True, "cookiesfrombrowser": ('chrome',), "playlistend": 1, "logger": ytdlp_logger}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydlp:
+        for channel in channels.values():
+            try:
+                print(f"[INFO] Looking through member's pages {counter}/{dict_length}...", end="\r")
 
-            info_dict = json.loads(process.stdout)
-
-            if info_dict['is_live']:
-                dict_list.append({"id": info_dict['id'], "title": info_dict['title'],
-                                  "channel": {"name": info_dict['channel']}})
-            else:
-                continue
-        except json.JSONDecodeError as jsonError:
-            logger.debug(str(jsonError) + f" occurred when looking through {channel}")
-        except Exception as e:
-            print(" " * 40, end="\n")
-            logger.error(e)
-        finally:
-            counter += 1
-            # time.sleep(4)
+                info = ydlp.extract_info(f"https://www.youtube.com/channel/{channel}/membership", download=False)
+                info_dict = ydlp.sanitize_info(info)
+                if info_dict.get('entries')[0].get('is_live'):
+                    dict_list.append({"id": info_dict['entries'][0]['id'], "title": info_dict['entries'][0]['title'],
+                                      "channel": {"name": info_dict['channel'], 'id': channel}})
+                else:
+                    continue
+            except json.JSONDecodeError as jsonError:
+                logger.debug(str(jsonError) + f" occurred when looking through {channel}")
+            except KeyError as kError:
+                logger.debug(kError)
+            except yt_dlp.utils.DownloadError as dError:
+                logger.debug(dError)
+            except Exception as e:
+                logger.debug(str(e) + " "*50, exc_info=True)
+            finally:
+                counter += 1
     return dict_list
 
 
-def get_links():
-    other_channel_ids = const.other_channel_ids
-    api_key = const.API_KEY
-    if other_channel_ids is not None:
-        channel_ids = "%2".join(other_channel_ids)
-    email_links = []
+def notify(channel, id, session):
     try:
-        if const.FETCH_FROM_EMAIL:
-            email_links = member_links()
-            logger.debug(email_links)
-    except Exception as e:
-        # print(" " * 40, end="\n")
-        logger.error(e)
-
-    if const.FETCH_FROM_YTDL:
-        member_page_links = get_latest_member_streams()
-
-    # Use holodex api to grab live streams
-    req = []
-    req2 = []
-    if const.FETCH_FROM_HOLODEX:
-        try:
-            req = requests.get(url="https://holodex.net/api/v2/live?org=Hololive&status=live").json()
-        except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
-            logger.error(rerror)
-            logger.error("Something went wrong sending the request")
-        try:
-            if other_channel_ids is not None or len(other_channel_ids) > 0:
-                if api_key is not None:
-                    req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
-                else:
-                    req2 = requests.get(url=f"https://holodex.net/api/v2/users/live?channels={channel_ids}", headers={"X-APIKEYS": api_key}).json()
-        except (requests.exceptions.RequestException, json.decoder.JSONDecodeError) as rerror:
-            logger.error(rerror)
-            logger.error("Something went wrong sending the request")
-
-    combined_data = req + req2 + email_links
-    streams = []
-    if len(combined_data) != 0:
-        # TODO: download info-json using yt-dlp or save json like auto-ytarchive
-        logger.debug(combined_data)
-        for stream in combined_data:
-            logger.debug(stream)
-            # Check the html page to see if it's a member stream
-            # if get_is_member_stream(stream["id"]):
-            #     steams.append(stream)
-            # Return name of the stream in lowercase
-            stream_name = stream['title'].lower()
-            logger.debug(f"Stream name: {stream_name}")
-            member_only = get_is_member_stream(stream['id'])
-            logger.debug(member_only)
-            if re.search('(member|members)', stream_name) or re.search('(メンバ|メン限)', stream_name) or member_only:
-                streams.append(stream)
-    streams += member_page_links
-    return streams
-
-
-def notify(name, id):
-    try:
-        channel_url = "https://www.youtube.com/channel/" + channels[name]
+        channel_url = "https://www.youtube.com/channel/" + channel[1]
         url = f"https://youtu.be/{id}"
-        message = f"[{name}](<{channel_url}>) has a member-only stream live at {url}"
-        requests.post(WEBHOOK_URL, json={'content': message})
+        message = f"[{channel[0]}](<{channel_url}>) has a member-only stream live at {url}"
+        session.post(WEBHOOK_URL, json={'content': message})
+    except requests.exceptions.HTTPError as hError:
+        logger.error(hError, exc_info=True)
+    except requests.exceptions.RequestException as rException:
+        logger.error(rException, exc_info=True)
+    except KeyError as kError:
+        logger.error(kError)
+        logger.debug(channels)
     except Exception as e:
-        # print(" " * 40, end="\n")
-        logger.error(e)
+        logger.error(e, exc_info=True)
 
 
-def get_is_member_stream(video_id):
-    url = f"https://www.youtube.com/watch?v={video_id}"
+def notify_dead(message, session):
     try:
-        # req = urllib.request.Request(url).text
-        req = requests.get(url).text
-        # logger.debug(req)
-        if '"offerId":"sponsors_only_video"' in req:
-            return True
-        else:
-            return False
+        session.post(DEAD_MEMBER_WEBHOOK_URL, json={'content': message})
     except Exception as e:
-        # print(" " * 40, end="\n")
         logger.error(e)
-        return False
 
 
 def download():
-    streams = get_links()
+    streams = get_latest_member_streams()
     for stream in streams:
-        streamer_name = stream["channel"]["name"]
+        streamer = (stream["channel"]["name"], stream["channel"]["id"])
         stream_id = stream["id"]
         try:
-            import getjson
+            import util
             url = f"https://youtu.be/{stream_id}"
-            getjson.get_json(url)
+            util.get_json(url)
+        except (ImportError, ModuleNotFoundError):
+            pass
         except Exception as e:
             logger.error(e)
         # TODO    Take a look and change if statement later
@@ -217,18 +160,20 @@ def download():
             fetched[stream["channel"]["name"]] = {stream["id"]: {
                                                   'downloaded': 'false',
                                                   'notified': 'false',
+                                                  'status': 'OK',
                                                   'timestamp': time.time()}}
         elif stream["id"] not in fetched[stream["channel"]["name"]].keys():
             fetched[stream["channel"]["name"]][stream["id"]] = {
                                                   'downloaded': 'false',
                                                   'notified': 'false',
+                                                  'status': 'OK',
                                                   'timestamp': time.time()}
         try:
             if WEBHOOK_URL is not None and fetched[stream["channel"]["name"]][stream["id"]]["notified"] != 'true':
-                notify(streamer_name, stream_id)
+                notify(streamer, stream_id, session)
                 fetched[stream["channel"]["name"]][stream["id"]]["notified"] = 'true'
         except KeyError:
-            notify(streamer_name, stream_id)
+            notify(streamer, stream_id, session)
             fetched[stream["channel"]["name"]][stream["id"]]["notified"] = 'true'
     save()
     # Get video_id's that have not been downloaded
@@ -241,7 +186,17 @@ def download():
     if len(download_id) != 0:
         logger.debug(download_id)
         download_result = live_download.download(download_id)
-
+        try:
+            import util
+            import threading
+            if const.CHAT_PATH is not None:
+                for d_id in download_id:
+                    threading.Thread(target=util.get_chat, args=[d_id[1]]).start()
+        except (ImportError, ModuleNotFoundError, NameError):
+            pass
+        except Exception as e:
+            logger.error(e)
+            pass
         for stream_tuple in download_id:
             streamer = stream_tuple[0]
             stream_id = stream_tuple[1]
@@ -259,16 +214,59 @@ def download():
         save()
     else:
         # TODO maybe put statement before log
-        logger.info("No member's only stream found/downloaded")
-        # print(" " * 40, end="\n")
+        logger.debug("No member's only stream found/downloaded")
+        # print(" " * 40, end="\r")
         save()
+
+
+def check_status(session):
+    if len(fetched) != 0:
+        global stop_loading
+        stop_loading = False
+        threading.Thread(target=loading_text).start()
+        url = "https://www.youtube.com/watch?v="
+        save_change = False
+        for channel in fetched:
+            for id in fetched[channel]:
+                try:
+                    res = session.get(url+id).text
+                except requests.exceptions as reqExceptions:
+                    logger.error(reqExceptions)
+                    continue
+                var = re.search(pattern="(var ytInitialPlayerResponse = )([^\n].*)", string=res)
+                if var is not None:
+                    res = var.group(2)
+                status = ""
+                copyright_message = ['copyright', 'copyrighted', 'Copyright', 'Copyrighted']
+                if '"status":"UNPLAYABLE"' in res and any(message in res for message in copyright_message):
+                    status = "Copyrighted"
+                elif '"Private video"' in res:
+                    status = "Privated"
+                elif '"status":"ERROR"' in res:
+                    status = "Removed"
+                elif '"isUnlisted":true"' in res:
+                    status = "Unlisted"
+
+                if fetched[channel][id]["status"] == "OK" and status != "":
+                    channel_url = "https://www.youtube.com/channel/" + channels[channel]
+                    url = f"https://youtu.be/{id}"
+                    message = f"Member-only stream from [{channel}](<{channel_url}>) is now `{status}` at {url}"
+                    logger.info(message)
+                    logger.debug(res[:3000])
+                    fetched[channel][id]["status"] = status
+                    save_change = True
+                    if const.DEAD_MEMBER_WEBHOOK_URL != "":
+                        notify_dead(message, session)
+        if save_change:
+            save()
+        stop_loading = True
 
 
 def sleeping_text():
     counter = sleep_time
-    print(" "*35, end="\n")
+    print(" "*70, end='\r')
     while True:
-        loading_string = f"[INFO] Sleeping for {counter} secs"
+        loading_string = f"[INFO] Sleeping for {counter} secs..."
         if counter == 0:
             break
         print(loading_string, end="\r")
@@ -276,27 +274,33 @@ def sleeping_text():
         time.sleep(1)
 
 
+def loading_text():
+    loading_string = "[INFO] Checking member's only stream status "
+    animation = ["     ", ".    ", "..   ", "...  ", ".... ", "....."]
+    idx = 0
+    while not stop_loading:
+        print(loading_string + animation[idx % len(animation)], end="\r")
+        time.sleep(0.3)
+        idx += 1
+        if idx == 6:
+            idx = 0
+
+
 if __name__ == '__main__':
     logger.info("Starting Program")
-    clear_link()
     expire_time = const.EXPIRE_TIME
     sleep_time = const.SLEEP_TIME
-    cleared = True
+    clear_link(expire_time)
+    session = requests.Session()
+    session.mount("https://", HTTPAdapter(max_retries=5))
     while True:
         try:
-            if cleared:
-                start_time = time.time()
-                cleared = False
-            if time.time() - start_time > expire_time:
-                clear_link()
-                start_time = time.time()
-                cleared = True
             download()
-            sleeping_text()
+            check_status(session)
+            if len(fetched) != 0:
+                clear_link(expire_time)
         except KeyboardInterrupt as k:
-            logger.error(k)
-            # print(" " * 40, end="\n")
+            logger.debug(k, exc_info=True)
         except Exception as e:
             logger.error(e, exc_info=True)
-            # print(" " * 40, end="\n")
             logger.debug(fetched)
